4fbd5b960ce8381e3c05de7dd9af7904a3ba5f27
Julian Ibarz <julian.ibarz@gmail.com>
2019-05-04 07:03:51 +0000

    Make server secure.

--- web/main.go
+++ web/main.go
@@ -26,19 +26,16 @@ import (
 
 func main() {
 	// use PORT environment variable, or default to 8080
-	port := "8080"
+	port := "443"
 	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
 		port = fromEnv
 	}
 
-	// register hello function to handle all requests
-	server := http.NewServeMux()
-	server.HandleFunc("/", hello)
-
-	// start the web server on port and accept requests
-	log.Printf("Server listening on port %s", port)
-	err := http.ListenAndServe(":"+port, server)
-	log.Fatal(err)
+    http.HandleFunc("/", HelloServer)
+    err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
 
 // hello responds to the request with a plain-text "Hello, world" message.
