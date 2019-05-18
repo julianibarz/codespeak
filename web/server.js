0adee1a9c603404d6d838c7b6fa9e8c39e5002ff
Julian Ibarz <julian.ibarz@gmail.com>
2019-05-18 16:14:15 -0400

    Remove chapp and upadte redirection.

--- web/server.js
+++ web/server.js
@@ -19,12 +19,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/codespeak', (req, res) => {
-  res.send('This is the url where I will put my CodeSpeak demo!');
-});
-
 app.get('/', (req, res) => {
-  res.redirect('https://ai.google/research/people/JulianIbarz');
+  res.send('Welcome on CodeSpeak demo!');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
