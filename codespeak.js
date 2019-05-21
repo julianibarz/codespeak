fdb45e2fc2de8945ab80de4aaa42d034d0b361d5
Julian Ibarz <julian.ibarz@gmail.com>
2019-05-20 20:39:02 -0400

    Add a sign in button.

--- codespeak.js
+++ codespeak.js
@@ -4,6 +4,14 @@ var term = new Terminal({cursorBlink: true, rows: 40});
 term.open(document.getElementById('terminal'));
 term.fit();
 
+$('#g-signin').on('data-onsuccess', function(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+});
+  
 function upgrade() {
   $(document.body).empty();
   var para = document.createElement('p');
