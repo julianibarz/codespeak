96f414b9219203e917b50f8e202c4e95a0eac0f1
Julian Ibarz <julian.ibarz@gmail.com>
2019-05-29 03:36:09 -0400

    Separate authentification logic in its own module.

---
+++ src/auth.js
@@ -0,0 +1,27 @@
+export function createAuthentification() {
+  function onSuccess(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('IDV: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+  gapi.signin2.render('g-signin', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+  $("#g-signout").click(function() {
+   var auth2 = gapi.auth2.getAuthInstance();
+   auth2.signOut().then(function() {
+     console.log('User signed out.');
+   });
+  });
+}
--- src/index.js
+++ src/index.js
@@ -5,17 +5,12 @@ import {
 import {
   createSpeechRecognition
 } from './speech.js';
+import {
+  createAuthentification
+} from './auth.js';
 
 $(function() {
   var term = createTerminal();
   var speech = createSpeechRecognition(term);
-
-  $('#g-signin').on('data-onsuccess', function(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  });
-
+  createAuthentification();
 });
